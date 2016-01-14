5d2ade6787a6f31806e9d134abf844f57e22d334
ehime <dodomeki@gmail.com>
2016-01-13 17:35:52 -0800

    Updated installer with some other docker stuff

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# REF : https://docs.docker.com/machine/
+# REF : https://docs.docker.com/v1.8/installation/mac/
+# REF : http://prakhar.me/docker-curriculum/
 
 brew update
-brew install docker
-brew install boot2docker
+brew install docker boot2docker docker-machine docker-compose busybox
+brew cask install kitematic --appdir=~/Applications
+
+mkdir -p ~/.docker/{machine,compose}
